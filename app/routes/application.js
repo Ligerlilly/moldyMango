c0d5692b7eb770ad4e24142c0ae6ff450bd081d9
Jason Lane & Vaughn Dice <student@epicodus.com>
2015-10-01 10:32:07 -0700

    refactor sign in

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+var ref = new Firebase("https://moldy-mangos.firebaseio.com");
+var signIn = function(provider, params) {
+  ref.authWithPassword({
+    email    : params.email,
+    password : params.password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      window.location.reload();
+    }
+  });
+}
 
 export default Ember.Route.extend({
   beforeModel: function() {
@@ -6,7 +20,6 @@ export default Ember.Route.extend({
   },
   actions: {
     signUp: function(params) {
-      var ref = new Firebase("https://moldy-mangos.firebaseio.com");
       ref.createUser({
         email    : params.email,
         password : params.password
@@ -15,33 +28,14 @@ export default Ember.Route.extend({
           console.log("Error creating user:", error);
         } else {
           console.log("Successfully created user account with uid:", userData.uid);
-          ref.authWithPassword({
-            email    : params.email,
-            password : params.password
-          }, function(error, authData) {
-            if (error) {
-              console.log("Login Failed!", error);
-            } else {
-              console.log("Authenticated successfully with payload:", authData);
-              window.location.reload();
-            }
-          });
         }
+
+        // auto sign in
+        signIn('password', params);
       });
     },
     signIn: function(provider, params) {
-      var ref = new Firebase("https://moldy-mangos.firebaseio.com");
-      ref.authWithPassword({
-        email    : params.email,
-        password : params.password
-      }, function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData);
-          window.location.reload();
-        }
-      });
+      signIn(provider, params);
     },
     signOut: function() {
       this.get("session").close();
